7f834c99cf9b804100621fc9d00715d16ee2c14d
RUPESHBORADE <rupesh.borade.39@gmail.com>
2023-03-11 12:10:50 +0530

    yes

---
+++ Datatype.java
@@ -0,0 +1,17 @@
+package Basicstarting;
+
+public class Datatype {
+
+	static void abc() {
+		int a = 10;
+		int b = 20;
+		int c = a + b;
+		System.out.println(c);
+	}
+
+	public static void main(String[] args) {
+
+		abc();
+	}
+
+}
---
+++ FIRSTCLASS.java
@@ -0,0 +1,9 @@
+package Basicstarting;     // package
+
+public class FIRSTCLASS {        // class name
+
+	public static void  main(String[]args) // main method 
+	{
+		System.out.println("rupesh");    // Hello world program....
+	}
+}
